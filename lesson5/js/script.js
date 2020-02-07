8c228f3f1c62a86cd49386c6e854f21f7c63df2a
melanyvlm <melany.svalencia@gmail.com>
2020-02-06 22:17:28 -0500

    page town continued half way

---
+++ lesson5/js/script.js
@@ -0,0 +1,14 @@
+// function toggleMenu() {
+// document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+
+// }
+
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+  document.querySelector(".navigation").classList.toggle("responsive");
+}
+
+let thisDocument  = new Date (document.lastModified) ;
+document.getElementById("showDateWeather").innerHTML = thisDocument;
