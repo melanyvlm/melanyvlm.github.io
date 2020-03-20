131ad14ea3f7666b14189712e4f5cd5e846992bd
melanyvlm <melany.svalencia@gmail.com>
2020-03-20 02:39:34 -0500

    api fix test 3

--- lesson11/js/forecast.js
+++ lesson11/js/forecast.js
@@ -1,6 +1,5 @@
 const forecastURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=bf83dc2f9a9872c97095e545db7c21ec&units=imperial";
 
-
 const daysoftheweek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 fetch(forecastURL)
   .then((response) => response.json())
@@ -14,8 +13,8 @@ fetch(forecastURL)
 
     for (let i = 0; i < fivedayforecast.length; i++) {
       document.getElementById(`forecast${i + 1}`).textContent = fivedayforecast[i].main.temp;
-      const img = `https://openweathermap.org/img/w/${fivedayforecast[i].weather[0].icon}.png`
-      document.getElementById(`img${i + 1}`).setAttribute('src', img);
+      // const img = `https://openweathermap.org/img/w/${fivedayforecast[i].weather[0].icon}.png`
+      // document.getElementById(`img${i + 1}`).setAttribute('src', img);
       const datetoday = new Date(fivedayforecast[i].dt_txt);
       const day = daysoftheweek[datetoday.getDay()];
       days[i].textContent = day;
