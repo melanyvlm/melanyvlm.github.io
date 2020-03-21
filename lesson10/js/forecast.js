848764418968877b81ec2054460a624519d8bad8
melanyvlm <melany.svalencia@gmail.com>
2020-03-21 15:42:25 -0500

    http access ?

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -1,4 +1,4 @@
-const forecastiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=bf83dc2f9a9872c97095e545db7c21ec&units=imperial";
+const forecastiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=bf83dc2f9a9872c97095e545db7c21ec&units=imperial";
 
 const daysoftheweek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 fetch(forecastiURL)
