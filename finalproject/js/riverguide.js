1b6092b476a15ced2e3bcded7fe3ada22cb465b4
melanyvlm <melany.svalencia@gmail.com>
2020-04-08 11:36:25 -0500

    river guides cards

--- finalproject/js/riverguide.js
+++ finalproject/js/riverguide.js
@@ -11,25 +11,25 @@ fetch(requestURL)
     console.table(jsonObject);  
     for (let i = 0; i < riverguides.length; i++ ) {
        let card = document.createElement('article');
-      //  let contain2 = document.createElement('div');
+       let contain2 = document.createElement('div');
        let contain = document.createElement('div');
      
        let titles = document.createElement('h3');  
-       let subtitle = document.createElement('h4');    
+       let subtitle = document.createElement('h3');    
        let p1 = document.createElement('p');    
        let p2 = document.createElement('p');    
        let p3 = document.createElement('p');    
+       let p4 = document.createElement('p');   
        
-       
-      //  contain2.setAttribute('class' , 'title');
-       titles.textContent =  riverguides[i].name;
-       subtitle.textContent =  riverguides[i].motto;
+     
+       titles.textContent =  riverguides[i].name + " " + riverguides[i].lastname;;
        let image = document.createElement('img');
-       contain.setAttribute('class' , 'container');
-       p1.textContent =  'Year Founded : ' + riverguides[i].yearFounded;
-       p2.textContent =  'Population : ' + riverguides[i].currentPopulation;
-       p3.textContent =  'Population : ' + riverguides[i].averageRainfall;
-       image.setAttribute('src', 'cities-img/'+ riverguides[i].photo);
+       contain.setAttribute('class' , 'guide-card');
+       p1.textContent =  'certification level : ' + riverguides[i].yearFounded;
+       p2.textContent =  'years of experience: ' + riverguides[i].currentPopulation;
+       p3.textContent =  'email address : ' + riverguides[i].averageRainfall;
+       p4.textContent =  ' biography : ' + riverguides[i].averageRainfall;
+       image.setAttribute('src', riverguides[i].image);
        image.setAttribute('alt', riverguides[i].name)
 
    card.appendChild(contain);
@@ -39,6 +39,7 @@ fetch(requestURL)
       contain.appendChild(p1);
       contain.appendChild(p2);
       contain.appendChild(p3);
+      contain.appendChild(p4);
         card.appendChild(image);
 
         document.querySelector('div.guides').appendChild(card); 
--- lesson11/js/forecast.js
+++ lesson11/js/forecast.js
@@ -1,22 +1,22 @@
-// const forecastiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=bf83dc2f9a9872c97095e545db7c21ec&units=imperial";
+const forecastiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=bf83dc2f9a9872c97095e545db7c21ec&units=imperial";
 
-// const daysoftheweek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-// fetch(forecastiURL)
-//   .then((response) => response.json())
-//   .then((jsObject) => {
-//     console.log(jsObject);
-//     const days = document.getElementsByClassName('label-day');
-//     const fivedayforecast = jsObject.list.filter(x =>
-//       x.dt_txt.includes('18:00:00'));
-//     console.log(fivedayforecast);
+const daysoftheweek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+fetch(forecastiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    const days = document.getElementsByClassName('label-day');
+    const fivedayforecast = jsObject.list.filter(x =>
+      x.dt_txt.includes('18:00:00'));
+    console.log(fivedayforecast);
 
 
-//     for (let i = 0; i < fivedayforecast.length; i++) {
-//       document.getElementById(`forecast${i + 1}`).textContent = fivedayforecast[i].main.temp;
-//       const img = `https://openweathermap.org/img/w/${fivedayforecast[i].weather[0].icon}.png`
-//       document.getElementById(`img${i + 1}`).setAttribute('src', img);
-//       const datetoday = new Date(fivedayforecast[i].dt_txt);
-//       const day = daysoftheweek[datetoday.getDay()];
-//       days[i].textContent = day;
-//     }
-//   });
+    for (let i = 0; i < fivedayforecast.length; i++) {
+      document.getElementById(`forecast${i + 1}`).textContent = fivedayforecast[i].main.temp;
+      const img = `https://openweathermap.org/img/w/${fivedayforecast[i].weather[0].icon}.png`
+      document.getElementById(`img${i + 1}`).setAttribute('src', img);
+      const datetoday = new Date(fivedayforecast[i].dt_txt);
+      const day = daysoftheweek[datetoday.getDay()];
+      days[i].textContent = day;
+    }
+  });
